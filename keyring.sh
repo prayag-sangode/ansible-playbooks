2bd9f7d0ee3157818ef14632a4eb34bb6cd4c507
Prayag Sangode <prayag.rhce@gmail.com>
2023-01-16 23:15:45 +0530

    update playbook

---
+++ keyring.sh
@@ -0,0 +1,7 @@
+# google-cloud-cli-gke-gcloud-auth-plugin
+
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+
+apt-get update && apt-get install google-cloud-cli-gke-gcloud-auth-plugin
